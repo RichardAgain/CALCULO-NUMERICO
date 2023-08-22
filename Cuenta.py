0badafed2c2438d9951a20b7e39c5f572aaeee2d
Ricardo Hernandez <142411594+RichardAgain@users.noreply.github.com>
2023-08-22 07:41:17 -0400

    Add files via upload

---
+++ Cuenta.py
@@ -0,0 +1,135 @@
+#
+# def ver_string(msg):
+#     res = input(msg)
+#     while res in []:
+#         pass
+#
+#     return res
+class cuenta:
+    usuarios = []   # Almacenamiento de los datos de usuarios
+
+    def __init__(self, ci, nombre, saldo) -> None:
+
+        self.ci = ci
+        self.nombre = nombre
+        self.saldo = saldo
+        cuenta.usuarios.append(self)
+
+        # Se crea el objeto con los atributos nombre, cedula y su saldo inicial
+
+        print("Se creo la cuenta satisfactoriamente\n")
+
+
+    def restar_saldo(self, num):
+        if self.saldo - num < 0:
+            print("Saldo insuficiente")
+            return False
+
+        self.saldo -= num
+        return True
+    # Metodo que recibe un objeto cuenta y resta su saldo - numero enviado
+    @classmethod
+    def existe_cuenta(cls, cedula):
+        for i in cls.usuarios:
+            if cedula == i.ci: return True
+        print("La cedula no coincide con la base de datos, intente de nuevo\n")
+        return False
+    # Metodo que verifica si la cedula enviada corresponde a una cuenta existente
+    @classmethod
+    def conseguir_cuenta(cls, cedula):
+        for usuario, i in zip(cls.usuarios, range(len(cls.usuarios))):
+            if cedula == usuario.ci: return cls.usuarios[i]
+    # Ciclo que recorre la lista de usuarios con un contador, cuando coincida
+    # una cedula con la lista devuelve es objeto usuario
+    @classmethod
+    def verificar_usuarios(cls, num):
+        if len(cls.usuarios) < num:
+            print("No existen suficientes usarios para realizar esta operacion, regresando\n")
+            return True
+        return False
+    # Recibe un numero y lo compara con la lista de usuarios, si el numero es mayor retorna False
+    # de lo contrario retorna True
+def crear_cuenta():
+    cedula = int(input("Ingrese la cedula: "))
+    nombre = input("Ingrese nombre de usuario: ")
+    saldo = int(input("Ingrese monto inicial: "))
+
+    cuenta(cedula, nombre, saldo)
+
+def consulta():
+    if cuenta.verificar_usuarios(1): return
+
+    cedula = int(input("Ingrese la cedula: "))
+
+    if cuenta.existe_cuenta(cedula):
+        co = cuenta.conseguir_cuenta(cedula)
+        print("\nNombre de Usuario: {}\nReferencia: {}\nSaldo: {}\n".format(co.nombre, co.ci, co.saldo))
+
+    else:
+        consulta()
+
+
+def retiro():
+    """retiro:"""
+    if cuenta.verificar_usuarios(1): return
+
+    cedula = int(input("Ingrese la cedula: "))
+    if cuenta.existe_cuenta(cedula):
+        re = int(input("Ingrese el monto a retirar: "))
+        co = cuenta.conseguir_cuenta(cedula)
+
+        if co.restar_saldo(re): print("Operacion exitosa, regresando...\n")
+    else:
+        retiro()
+
+def deposito():
+    if cuenta.verificar_usuarios(1): return
+
+    cedula = int(input("Ingrese la cedula: "))
+    if cuenta.existe_cuenta(cedula):
+        re = int(input("Ingrese el monto a depositar: "))
+        co = cuenta.conseguir_cuenta(cedula)
+
+        co.saldo += re
+        print("Operacion exitosa, regresando...\n")
+    else:
+        retiro()
+
+
+def transferencia():
+    if cuenta.verificar_usuarios(2): return
+
+    cedula1 = int(input("Ingrese la cedula de la cuenta a retirar: "))
+    if cuenta.existe_cuenta(cedula1) == False: transferencia()
+    cedula2 = int(input("Ingrese la cedula de la cuenta a recibir: "))
+
+    if cuenta.existe_cuenta(cedula2):
+        re = int(input("Ingrese el monto a transferir: "))
+        co1 = cuenta.conseguir_cuenta(cedula1)
+        co2 = cuenta.conseguir_cuenta(cedula2)
+
+        if co1.restar_saldo(re):
+            co2.saldo += re
+            print("Operacion exitosa, regresando...\n\n")
+
+    else:
+        transferencia()
+
+
+def main():
+    while True:
+        print("Banesco Online\n\n(1) Crear usuario\n(2) Consulta\n(3) Deposito\n(4) Retiro\n(5) Transferencia\n(6) Finalizar\n")
+
+        op = input("Marque una opcion: ")
+        while op not in ["1", "2", "3", "4", "5", "6"]:
+            print("Respuesta invalida, intente de nuevo")
+            op = input("Marque una opcion: ")
+
+        if op == "1": crear_cuenta()
+        if op == "2": consulta()
+        if op == "3": deposito()
+        if op == "4": retiro()
+        if op == "5": transferencia()
+        if op == "6": break
+
+main()
